3d2acaa9ff7cbea045fc54a422e3f89c335465a3
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2018-06-12 10:38:38 +0100

    Fixed no artist bug.

--- app/src/main/java/com/adetech/simila/adapter/SimilarArtistListAdapter.java
+++ app/src/main/java/com/adetech/simila/adapter/SimilarArtistListAdapter.java
@@ -3,6 +3,7 @@ package com.adetech.simila.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,18 +35,22 @@ public class SimilarArtistListAdapter extends RecyclerView.Adapter<SimilarArtist
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.custom_row, parent, false);
 
-        return  new CustomViewHolder(view);
+        return new CustomViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CustomViewHolder holder, int position)
     {
-        //TODO fix this .
-            holder.artistNameTxtView.setText( mDataList.get(position).getName());
-            holder.mArtistMatchRate.setText("Match Rate: " + " " +mDataList.get(position).getMatch());
-            //Uses picasso to load the first image of an artist.
-            Picasso.with(mContext).load(mDataList.get(position).getImage().get(2).getText()).placeholder(R.drawable.ic_launcher_background).into(holder.mArtistImgView);
+        //TODO fix this  use template..
+        //ToDo app crashes whn you search genre.
+        holder.artistNameTxtView.setText(mDataList.get(position).getName());
+        holder.mArtistMatchRate.setText("Match Rate: " + " " + mDataList.get(position).getMatch());
 
+        String artistImagePath = (mDataList.get(position).getImage().get(2).getText());
+        if (!TextUtils.isEmpty(artistImagePath))
+        {
+            Picasso.with(mContext).load(artistImagePath).placeholder(R.drawable.ic_launcher_background).into(holder.mArtistImgView);
+        }
     }
 
     @Override
@@ -55,9 +60,9 @@ public class SimilarArtistListAdapter extends RecyclerView.Adapter<SimilarArtist
     }
 
 
-     class CustomViewHolder extends  RecyclerView.ViewHolder
+    class CustomViewHolder extends RecyclerView.ViewHolder
     {
-        public final  View mView;
+        public final View mView;
 
         TextView artistNameTxtView;
         TextView mArtistMatchRate;
